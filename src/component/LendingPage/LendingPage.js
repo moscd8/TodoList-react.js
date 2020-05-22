9e956f03fd15648249acc1ba533ee2ff2afc7b00
mos <moswnd@gmail.com>
2020-05-22 03:14:04 +0300

    add css style to langing page

--- src/component/LendingPage/LendingPage.js
+++ src/component/LendingPage/LendingPage.js
@@ -1,28 +1,72 @@
 import React  from 'react';
 import classes from './LendingPage.module.css';
+import { faHome , faListAlt, faMoneyBillAlt, faClock, faGlobeAfrica, faTasks } from "@fortawesome/free-solid-svg-icons";
+import {  faFacebook,faInstagram,faTwitter,faGoogle,faYoutube, faWhatsapp } from '@fortawesome/free-brands-svg-icons';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
 const LendingPage = props => {
     
     return (
         <div className={classes.LendingPage}>
-            <p> MY TODO-LIST  </p> 
-            <div className={classes.About}>
-                 <p>About</p>
+            <div className={classes.Main}>
+                <p className={classes.Title}> MY TODO-LIST
+                    <FontAwesomeIcon icon={faListAlt} size='lg' color="white" pull="center"/>
+                </p> 
+                <div className={classes.About}>
+                 
                  <span>
+                 <p>About
+                     
+                     </p>
                      WITH TODOLIST YOU CAN ADD YOUR TODO ITEMS <br/>
                      AND STORE IT IN THE OUR SERVERS AND ACCESS IT <br/>ANYWHERE FOR FREE. 
                  </span>
-            </div>
-            <div className={classes.Benifits}>
-                <p>Benifits</p>
-                <li> <strong>Producative:</strong>  you can manage your time and track after your prograss
-                </li>
-                <li> <strong>Free:</strong>  you can use TODOLIST with no limit & without any Ads
-                </li>
-                <li>  <strong>Keep Truck :</strong> you can manage your TODOLIST anywhere online
-                </li>
-                <li>  <strong>Easy to Use: </strong> you can manage your time and track after your prograss
-                </li>
+                </div>
+                <a  className={classes.ReadMore} href={'#Benifits'}>Read More</a>
+               
+                {/* <button className={classes.ReadMore}>
+                 </button> */}
 
+            </div>                
+            
+            <div className={classes.Benifits_Container} id={'Benifits'}>
+                <p className={classes.Benifits_Container_Header}>Benifits</p><br/>
+             <div className={classes.Benifits}>
+                <div className={classes.Benifit_a}>
+                    {/* <FontAwesomeIcon icon={faListAlt} size='9px' color="white"/>  */}
+                    <p><strong>Producative</strong></p>  you can manage your time and track after your prograss
+                    {/* <br/> */}
+                    <div className={classes.Icon}>
+                        <FontAwesomeIcon icon={faClock} size='lg' color="#7f7b8f"/> 
+                    </div>
+
+                </div>
+
+                <div className={classes.Benifit_a}>
+                    <p><strong>Free</strong></p> 
+                     you can use TODOLIST with no limit & without any Ads
+                     <div className={classes.Icon}>
+                        <FontAwesomeIcon icon={faMoneyBillAlt} size='lg' color="greed"/> 
+                    </div>
+                </div>
+
+                <div className={classes.Benifit_a}>
+                   <p><strong>Keep Truck </strong></p> 
+                   you can manage your TODOLIST anywhere online
+                   <div className={classes.Icon}>
+                        <FontAwesomeIcon icon={faGlobeAfrica} size='lg' color="white"/> 
+                    </div>
+                </div>
+
+                <div className={classes.Benifit_a}>
+                    <p><strong>Easy to Use </strong> </p>
+                    you can manage your time and track after your prograss
+                    <div className={classes.Icon}>
+                        <FontAwesomeIcon icon={faTasks} size='lg' color="#7f7b8f"/> 
+                    </div>
+                </div>
+    
+              </div>
             </div>
 
             <div className={classes.Recommndetion}>
@@ -40,6 +84,27 @@ const LendingPage = props => {
 
             <div className={classes.SocialNetwork}>
                 <p>SocialNetwork</p>
+                <div className={classes.SocialLinks}>
+                
+                <a href={'#a'}>
+                <FontAwesomeIcon icon={faFacebook} />
+                </a>
+                <a href={'#a'}>
+                <faTwitter icon={faTwitter}/>
+                </a>
+                <a href={'#a'}>
+                <FontAwesomeIcon icon={faInstagram}/>
+                </a>
+                <a href={'#a'}>
+                <FontAwesomeIcon icon={faGoogle} />
+                </a>
+                <a href={'#a'}>
+                <FontAwesomeIcon icon={faYoutube} />
+                </a>
+                <a href={'#a'}>
+                <FontAwesomeIcon icon={faWhatsapp} />
+                </a> 
+                </div>
             </div>
         </div>
     );
